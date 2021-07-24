11b7ff98f1fb5785593c974b1a42aeaa87e40d72
Eduardo Zamora <ezamora@devsalsa.com>
2021-07-24 14:56:02 -0500

    new app

---
+++ src/components/MenuApp.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header
+            mdl-layout--fixed-tabs">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <!-- Title -->
+        <span class="mdl-layout-title">{{ nameApp }}</span>
+      </div>
+      <!-- Tabs -->
+      <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
+        <a href="#fixed-tab-1" class="mdl-layout__tab is-active">Storages</a>
+        <a href="#fixed-tab-2" class="mdl-layout__tab">Tab 2</a>
+        <a href="#fixed-tab-3" class="mdl-layout__tab">Tab 3</a>
+      </div>
+    </header>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">{{ nameApp }}</span>
+      <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <section class="mdl-layout__tab-panel is-active" id="fixed-tab-1">
+        <div class="page-content">
+
+          <div class="container">
+            <br>
+            <br>
+            <h2 class="text-center titleModule"><img width="40"  src="../assets/database.png"> Storages</h2>
+            <div class="row">
+                <code>
+                  {{ typeof $data }}
+                </code>
+            </div>
+            <div class="row">
+              <div class="col-3">
+               <card-storage :storage="$data">
+
+               </card-storage>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="mdl-layout__tab-panel" id="fixed-tab-2">
+        <div class="page-content"><!-- Your content goes here --></div>
+      </section>
+      <section class="mdl-layout__tab-panel" id="fixed-tab-3">
+        <div class="page-content"><!-- Your content goes here --></div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+import cardStorage from "@/components/cardStorage";
+import axios from "axios";
+export default {
+  name: "MenuApp",
+  components: {
+    cardStorage
+  },
+  props: {
+    nameApp: String
+  },
+  data : function () {
+    axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.info = response))
+  }
+}
+</script>
+
+<style scoped>
+.demo-layout-waterfall .mdl-layout__header-row .mdl-navigation__link:last-of-type  {
+  padding-right: 0;
+}
+.titleModule {
+  font-weight: lighter !important;
+  color: #5f5f5f;
+}
+</style>
---
+++ src/components/cardStorage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card-storage demo-card-square mdl-card mdl-shadow--2dp">
+    <div style="margin-bottom: -20px" class="mdl-card__title mdl-card--expand">
+      <h2 class="mdl-card__title-text">{{ Object.keys(storage) }} <div style="margin-top: 5px; margin-left:10px;border-radius: 150%; height: 15px; width:15px; background:#42b983;"></div> </h2>
+    </div>
+    <div class="mdl-card__supporting-text">
+      <p class="ip-text">192.178.2.1 </p>
+      <hr>
+      <p class="plots-text">
+        Plots <strong>1323</strong>   / Disk space <strong>522TB</strong>
+      </p>
+    </div>
+    <div class="mdl-card__actions mdl-card--border">
+      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+
+      </a>
+    </div>
+  </div>
+</template>
+#5376
+<script>
+export default {
+  name: "cardStorage",
+  props: {
+    storage: Object
+  }
+}
+</script>
+
+<style scoped>
+.card-storage {
+  border-radius: 15px;
+}
+.ip-text{
+  font-size: 16px;
+  color: #18d3ec;
+  margin-top: -5px;
+  margin-bottom: -10px;
+}
+.plots-text{
+  font-size:16px;
+  margin-top: 10px;
+}
+</style>
